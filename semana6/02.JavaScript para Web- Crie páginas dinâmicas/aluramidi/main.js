9251fea807bb52f8cefb90944351db9396e9c40a
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-03-08 20:18:15 -0300

    added For loop structure

--- semana6/02.JavaScript para Web- Crie páginas dinâmicas/aluramidi/main.js
+++ semana6/02.JavaScript para Web- Crie páginas dinâmicas/aluramidi/main.js
@@ -1,8 +1,15 @@
-// Pom ----------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-listaDeTeclas[0].onclick = tocaSomPom;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+}
