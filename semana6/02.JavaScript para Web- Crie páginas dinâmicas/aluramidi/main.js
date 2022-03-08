13c9fcf7d6452099b0d904a26fc5db5bb03e7395
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-03-08 18:48:44 -0300

    added querySelectorAll

--- semana6/02.JavaScript para Web- Crie páginas dinâmicas/aluramidi/main.js
+++ semana6/02.JavaScript para Web- Crie páginas dinâmicas/aluramidi/main.js
@@ -1,5 +1,8 @@
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play();
+// Pom ----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector(".tecla_pom").onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
