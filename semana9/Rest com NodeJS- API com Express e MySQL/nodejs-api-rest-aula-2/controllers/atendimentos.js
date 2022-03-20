5336fee88f6473a4034db93a7a68acb6e45bb0ed
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-03-20 18:44:42 -0300

    upload aula-2

---
+++ semana9/Rest com NodeJS- API com Express e MySQL/nodejs-api-rest-aula-2/controllers/atendimentos.js
@@ -0,0 +1,11 @@
+module.exports = (app) => {
+  app.get("/atendimentos", (req, res) =>
+    res.send("Você está na rota de atendimentos e está realizando um GET")
+  );
+
+  app.post("/atendimentos", (req, res) => {
+    console.log("Atendimento enviado");
+    console.log(req.body);
+    res.send("Post atendimento");
+  });
+};
