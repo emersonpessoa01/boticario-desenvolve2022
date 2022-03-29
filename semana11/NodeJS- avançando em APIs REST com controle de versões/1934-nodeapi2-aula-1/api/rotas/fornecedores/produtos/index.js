09beb4248ecc5ca705e3c69ad5b9ec990c26d1d6
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-03-29 16:58:09 -0300

    upload projeto-inicial and aula-1

---
+++ semana11/NodeJS- avançando em APIs REST com controle de versões/1934-nodeapi2-aula-1/api/rotas/fornecedores/produtos/index.js
@@ -0,0 +1,9 @@
+const roteador = require("express").Router({ mergeParams: true });
+const Tabela = require("./TabelaProduto");
+
+roteador.get("/", async (requisicao, resposta) => {
+  const produtos = await Tabela.listar(requisicao.params.idFornecedor);
+  resposta.send(JSON.stringify(produtos));
+});
+
+module.exports = roteador;
