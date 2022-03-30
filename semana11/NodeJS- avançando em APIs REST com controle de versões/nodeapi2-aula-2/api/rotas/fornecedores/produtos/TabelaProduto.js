ec504711a9dcd2604ad5805e37448d452b82e0ba
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-03-30 10:53:02 -0300

    updated file name

---
+++ semana11/NodeJS- avançando em APIs REST com controle de versões/nodeapi2-aula-2/api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -0,0 +1,22 @@
+const Modelo = require("./ModeloTabelaProduto");
+
+module.exports = {
+  listar(idFornecedor) {
+    return Modelo.findAll({
+      where: {
+        fornecedor: idFornecedor,
+      },
+    });
+  },
+  inserir(dados) {
+    return Modelo.create(dados);
+  },
+  remover(idProduto, idFornecedor) {
+    return Modelo.destroy({
+      where: {
+        id: idProduto,
+        fornecedor: idFornecedor,
+      },
+    });
+  },
+};
---
+++ semana11/NodeJS- avançando em APIs REST com controle de versões/nodeapi2-aula-2/api/rotas/fornecedores/produtos/index.js
@@ -0,0 +1,36 @@
+const roteador = require("express").Router({ mergeParams: true });
+const Tabela = require("./TabelaProduto");
+const Produto = require("./Produto");
+
+roteador.get("/", async (requisicao, resposta) => {
+  const produtos = await Tabela.listar(requisicao.fornecedor.id);
+  resposta.send(JSON.stringify(produtos));
+});
+
+roteador.post("/", async (requisicao, resposta, proximo) => {
+  try {
+    const idFornecedor = requisicao.fornecedor.id;
+    const corpo = requisicao.body;
+    const dados = Object.assign({}, corpo, { fornecedor: idFornecedor });
+    const produto = new Produto(dados);
+    await produto.criar();
+    resposta.status(201);
+    resposta.send(produto);
+  } catch (erro) {
+    proximo(erro);
+  }
+});
+
+roteador.delete("/:id", async (requisicao, resposta) => {
+  const dados = {
+    id: requisicao.params.id,
+    fornecedor: requisicao.fornecedor.id,
+  };
+
+  const produto = new Produto(dados);
+  await produto.apagar();
+  resposta.status(204);
+  resposta.end();
+});
+
+module.exports = roteador;
