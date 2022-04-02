e6b62a73a8d177ed33debabe002beebdaca30e36
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-04-02 13:21:28 -0300

    upload aula-3

---
+++ semana11/02.ORM com NodeJS- API com Sequelize e MySQL/orm-sequelize-projeto-aula-3/api/controllers/PessoaController.js
@@ -0,0 +1,38 @@
+const database = require("../models");
+
+class PessoaController {
+  static async pegaTodasAsPessoas(req, res) {
+    try {
+      const todasAsPessoas = await database.Pessoas.findAll();
+      return res.status(200).json(todasAsPessoas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async pegaUmaPessoa(req, res) {
+    const { id } = req.params;
+    try {
+      const umaPessoa = await database.Pessoas.findOne({
+        where: {
+          id: Number(id),
+        },
+      });
+      return res.status(200).json(umaPessoa);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async criaPessoa(req, res) {
+    const novaPessoa = req.body;
+    try {
+      const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+      return res.status(200).json(novaPessoaCriada);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+}
+
+module.exports = PessoaController;
---
+++ semana11/02.ORM com NodeJS- API com Sequelize e MySQL/orm-sequelize-projeto-aula-3/api/routes/pessoasRoute.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const PessoaController = require("../controllers/PessoaController");
+
+const router = Router();
+
+router.get("/pessoas", PessoaController.pegaTodasAsPessoas);
+router.get("/pessoas/:id", PessoaController.pegaUmaPessoa);
+router.post("/pessoas", PessoaController.criaPessoa);
+
+module.exports = router;
