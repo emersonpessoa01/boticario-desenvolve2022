225af92e6bd84a6bf10c912152e0f5c94c2db944
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-04-05 08:21:04 -0300

    upload aula-1

---
+++ semana12/01.ORM com NodeJS avançando nas funcionalidades do Sequelize/sequelize-aula-1/sequelize-aula-1/api/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const routes = require("./routes");
+
+const app = express();
+const port = 3000;
+
+routes(app);
+
+app.listen(port, () =>
+  console.log(`servidor está rodando na porta http://localhost:${port}/pessoas`)
+);
+
+module.exports = app;
---
+++ semana12/01.ORM com NodeJS avançando nas funcionalidades do Sequelize/sequelize-aula-1/sequelize-aula-1/api/routes/pessoasRoute.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+const PessoaController = require("../controllers/PessoaController");
+
+const router = Router();
+
+router
+  .get("/pessoas", PessoaController.pegaTodasAsPessoas)
+  .get("/pessoas/:id", PessoaController.pegaUmaPessoa)
+  .post("/pessoas", PessoaController.criaPessoa)
+  .put("/pessoas/:id", PessoaController.atualizaPessoa)
+  .delete("/pessoas/:id", PessoaController.apagaPessoa)
+  .get(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.pegaUmaMatricula
+  )
+  .post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula)
+  .post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
+  .put(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.atualizaMatricula
+  )
+  .delete(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.apagaMatricula
+  )
+  .post(
+    "/pessoas/:estudanteId/matricula/:matriculaId/restaura",
+    PessoaController.restauraMatricula
+  );
+
+module.exports = router;
