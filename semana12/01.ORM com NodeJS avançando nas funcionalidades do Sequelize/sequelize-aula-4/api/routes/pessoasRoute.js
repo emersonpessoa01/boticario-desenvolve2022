e086014abb63ecbc36836731aed21eee77c1a892
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-04-06 22:00:17 -0300

    aula-4

---
+++ semana12/01.ORM com NodeJS avançando nas funcionalidades do Sequelize/sequelize-aula-4/api/routes/pessoasRoute.js
@@ -0,0 +1,39 @@
+const { Router } = require("express");
+const PessoaController = require("../controllers/PessoaController");
+
+const router = Router();
+
+router
+  .get("/pessoas", PessoaController.pegaPessoasAtivas)
+  .get("/pessoas/todos", PessoaController.pegaTodasAsPessoas)
+  .get("/pessoas/:id", PessoaController.pegaUmaPessoa)
+  .get(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.pegaUmaMatricula
+  )
+  .get("/pessoas/:estudanteId/matricula", PessoaController.pegaMatriculas)
+  .get(
+    "/pessoas/matricula/:turmaId/confirmadas",
+    PessoaController.pegaMatriculasPorTurma
+  )
+  .get("/pessoas/matricula/lotada", PessoaController.pegaTurmasLotadas)
+  .post("/pessoas", PessoaController.criaPessoa)
+  .post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula)
+  .post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
+  .post(
+    "/pessoas/:estudanteId/matricula/:matriculaId/restaura",
+    PessoaController.restauraMatricula
+  )
+  .post("/pessoas/:estudanteId/cancela", PessoaController.cancelaPessoa)
+  .put("/pessoas/:id", PessoaController.atualizaPessoa)
+  .put(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.atualizaMatricula
+  )
+  .delete("/pessoas/:id", PessoaController.apagaPessoa)
+  .delete(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.apagaMatricula
+  );
+
+module.exports = router;
