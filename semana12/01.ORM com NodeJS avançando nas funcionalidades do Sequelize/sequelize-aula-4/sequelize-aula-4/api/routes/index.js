20c0842555d2fa05df0fd2e7a4053e7bfff11638
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-04-07 17:06:12 -0300

    updated

---
+++ semana12/01.ORM com NodeJS avançando nas funcionalidades do Sequelize/sequelize-aula-4/sequelize-aula-4/api/routes/index.js
@@ -0,0 +1,9 @@
+const bodyParser = require("body-parser");
+
+const pessoas = require("./pessoasRoute");
+const niveis = require("./niveisRoute");
+const turmas = require("./turmasRoute");
+
+module.exports = (app) => {
+  app.use(bodyParser.json(), pessoas, niveis, turmas);
+};
