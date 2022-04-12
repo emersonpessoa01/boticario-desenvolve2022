1caa7d32bd9d40d37c565ec31e9805da936b5446
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2022-04-12 08:34:47 -0300

    updated package.json

--- semana10/02.NodeJS- crie uma API REST padronizada e escalável/api-petshop-aula-1/api/index.js
+++ semana10/02.NodeJS- crie uma API REST padronizada e escalável/api-petshop-aula-1/api/index.js
@@ -3,12 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 const roteador = require("./rotas/fornecedores");
 const config = require("config");
-const tabela = require("./banco-de-dados/criarTabelas")
+// const tabela = require("./banco-de-dados/criarTabelas")
 
 app.use(bodyParser.json());
 
 app.use("/api/fornecedores", roteador);
-app.use("/api/banco-de-dados/criarTabelas", tabela);
+// app.use("/api/banco-de-dados/criarTabelas", tabela);
 
 app.listen(config.get("api.porta"), () =>
   console.log("A API está funcionando!")
